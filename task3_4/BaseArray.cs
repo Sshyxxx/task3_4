8bf5925e92f967d82d3f2e3bcc19713095659a62
Александра Гаврина <alexgav2009@mail.ru>
2024-02-16 20:50:36 +0300

    Добавьте файлы проекта.

---
+++ task3_4/BaseArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3_4
+{
+    internal abstract class BaseArray : IArray, IPrinter
+    {
+        /// <summary>
+        /// Заполнение массива целыми числами
+        /// </summary>     
+        public abstract void GetIntValues();
+
+
+        /// <summary>
+        /// Заполнение массива случайными числами
+        /// </summary>     
+        public abstract void GetRandomValues();
+
+        /// <summary>
+        /// Выводит на экран массив
+        /// </summary>    
+        public abstract void Print();
+
+
+        /// <summary>
+        /// Считает среднее значение массива
+        /// </summary>
+        /// <returns>Дробное число, сумма, поделенная на количество элементов</returns>
+        public abstract double AverageValue();
+
+    }
+}
---
+++ task3_4/OneDArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3_4
+{
+    internal class OneDArray<T> : BaseArray
+    {
+        private T[] _array;
+        private int _size;
+
+        public OneDArray(int size, bool isUser = false)
+        {
+            _size = size;
+            _array = new T[_size];
+
+            if (!isUser)
+            {
+                GetRandomValues();
+            }
+            else
+            {
+                //GetIntValues();
+            }
+        }
+
+        public override double AverageValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetIntValues<T>()
+        {
+                //Console.WriteLine("введите " + _size + " элементов ");
+                //for (int i = 0; i < _array.Length; i++)
+                //{
+                //    Console.WriteLine($"элемент № {i}");
+                //    //_array[i] = int.Parse(Console.ReadLine());
+                //    _array[i] = Console.ReadLine();
+                //}
+        }
+
+
+        public override void GetRandomValues()
+        {
+            Random random = new Random();
+            for (int i = 0; i < _size; i++)
+            {
+               //_array[i] = random.Next(0, 255);
+            }
+        }
+
+        public override void Print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
